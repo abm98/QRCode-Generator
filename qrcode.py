76da59a19d4a888aad8f87a1fdd0df55e6c15a93
Abubakr Masood <68956896+abm98@users.noreply.github.com>
2021-02-19 16:36:50 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,5 +1,7 @@
 import pyqrcode
 import png
+# Provide the link below of any site which you want to convert in QR Code 
 image = pyqrcode.create("https://youtu.be/09fpBobFKqQ")
 image.svg("rekhta.svg")
 image.png("rekhta.png")
+# Save and Run You Will get the file in the same location where your py file is save
